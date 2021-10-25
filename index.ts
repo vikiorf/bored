422ce7e843e12280e5298241288060f436eed8dc
Viktor <viktor.johansson96@gmail.com>
2021-10-25 15:40:17 +0200

    added some classes

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 class Api {
-  async fetchRandomActivity() {
-    return await fetch('http://www.boredapi.com/api/activity/')
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res)
-    })
+	async fetchRandomActivity() {
+		return await fetch('http://www.boredapi.com/api/activity/')
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res)
+			})
+	}
+}
+
+class BoredItems {
+  static boredItem: object[]
+
+  addItem(item: object) {
+    BoredItems.boredItem.push(item)
   }
 }
 
+class Tasks {}
 
+class Render {}
 
 let api = new Api()
 let hej = api.fetchRandomActivity()
